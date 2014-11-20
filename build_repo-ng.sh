7eeb38330903393c773cae274abcf0fb3fd54638
Junxiao Shi <git@mail1.yoursunny.com>
2014-11-19 21:49:29 -0700

    add repo-ng

---
+++ build_repo-ng.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p repos/repo-ng
+cd repos/repo-ng
+
+echo Checking out from Gerrit: repo-ng master
+git init
+git fetch --depth=1 http://gerrit.named-data.net/repo-ng && git checkout FETCH_HEAD
+git submodule update --init
+
+echo Building repo-ng
+./waf configure --with-tests --with-examples --debug
+./waf -j4
+
+echo Running repo-ng tests
+build/unit-tests -l test_suite

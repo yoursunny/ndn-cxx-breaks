9763ea4f6a8bd0b879a580a60a1f1d50bf43bbcc
Junxiao Shi <git@mail1.yoursunny.com>
2014-11-19 21:39:47 -0700

    move pkg-config dependency to ndn-cxx

--- deps_ndn-cxx.sh
+++ deps_ndn-cxx.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-sudo apt-get install -qq libboost1.48-all-dev libcrypto++-dev libsqlite3-dev
+sudo apt-get install -qq libboost1.48-all-dev libcrypto++-dev libsqlite3-dev pkg-config

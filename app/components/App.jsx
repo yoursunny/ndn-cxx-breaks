badb4af20347a23b9051b116bec239ee02a07a31
Junxiao Shi <git@mail1.yoursunny.com>
2017-05-02 21:39:31 -0700

    static input box

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [
+        {
+          name: 'ndn-cxx'
+        },
+        {
+          name: 'NFD'
+        },
+        {
+          name: 'ndn-tools'
+        }
+      ]
+    };
+  }
+
   render() {
+    const projectRows = this.state.projects.map(proj =>
+      <ProjectRow key={proj.name} name={proj.name}/>
+    );
+
     return (
-      <div id="content">
-        <h5>Time to <a href="https://facebook.github.io/react/">React</a>.</h5>
-      </div>
+      <table>
+        <thead>
+          <tr><th>project</th><th>patchset</th></tr>
+        </thead>
+        <tbody>
+          {projectRows}
+        </tbody>
+      </table>
     );
   }
+}
+
+function ProjectRow(props) {
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td><input type="text" value={props.value} onChange={props.onChange}/></td>
+    </tr>
+  );
 }

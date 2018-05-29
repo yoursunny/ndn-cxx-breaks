88b3e0fbd66f74c4d56fc427c017e1359adb5422
Junxiao Shi <git@mail1.yoursunny.com>
2018-05-29 16:51:50 +0000

    travis: move build_ndn-cxx into 'install' step

--- make-Dockerfile.sh
+++ make-Dockerfile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+if [[ -z $DEPS ]]; then
+  DEPS=$(sort -u *.deps)
+else
+  DEPS=$(sort -u ndn-cxx.deps $PROJECT.deps)
+fi
 
 (
   echo FROM ubuntu:18.04
   echo WORKDIR /root
-  echo 'RUN apt-get update && apt-get install -qq' $(sort -u ndn-cxx.deps $PROJECT.deps)
+  echo 'RUN apt-get update && apt-get install -qq' $DEPS
   export | grep PATCHSET > patchset.env
   echo 'ADD build_*.sh checkout.sh patchset.env /root/'
   echo 'RUN ["/bin/bash", "-c", "source patchset.env; ./build_ndn-cxx.sh"]'
-  echo 'RUN ["/bin/bash", "-c", "source patchset.env; ./build_'$PROJECT'.sh"]'
-) > Dockerfile
+) | tee Dockerfile

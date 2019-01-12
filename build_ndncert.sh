c3ceda1e697c2293e6ce15dd9e3cc200ac043ff1
Davide Pesavento <davidepesa@gmail.com>
2019-01-12 16:27:31 -0500

    Add ndncert

---
+++ build_ndncert.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p ndncert
+cd ndncert
+
+../checkout.sh NDNCERT ndncert
+
+echo Building ndncert
+./waf configure --with-tests --debug
+./waf -j4 || ./waf -j1
+
+echo Running ndncert tests
+LD_LIBRARY_PATH=build build/unit-tests -l test_suite

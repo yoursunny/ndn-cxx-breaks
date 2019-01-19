a5ab87fcb4b3c96871973a6605605dafe870fbda
Davide Pesavento <davidepesa@gmail.com>
2019-01-18 20:25:17 -0500

    checkout.sh: support repos with default branch != "master"

--- checkout.sh
+++ checkout.sh
@@ -13,14 +13,15 @@ PATCHSETVAR=PATCHSET_${PROJVAR}
 PATCHSET=${!PATCHSETVAR:-master}
 
 if [[ ${PATCHSET} == master ]]; then
-  GERRIT_BRANCH=master
+  # use default branch
+  GERRIT_BRANCH=
 else
   GERRIT_CHANGE=${PATCHSET%,*}
   GERRIT_BRANCH=refs/changes/$(printf '%02d' $((GERRIT_CHANGE % 100)))/${GERRIT_CHANGE}/${PATCHSET#*,}
 fi
 
-echo Checking out $REPO $GERRIT_BRANCH
+echo "Checking out $REPO ${GERRIT_BRANCH:-(default branch)}"
 git init --quiet
-git fetch --quiet --depth=1 $REPO $GERRIT_BRANCH
+git fetch --quiet --depth=1 "$REPO" "$GERRIT_BRANCH"
 git -c advice.detachedHead=no checkout FETCH_HEAD
 git submodule update --init

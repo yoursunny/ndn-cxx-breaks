4335967b5111d069792f62589d16776a0ebbb51f
Davide Pesavento <davidepesa@gmail.com>
2022-04-21 19:24:26 -0400

    checkout.sh: minor simplifications

--- checkout.sh
+++ checkout.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -eo pipefail
-PROJ=$1
 
+PROJ=$1
 PROJVAR=${PROJ//-}
 PROJVAR=${PROJVAR^^}
 PATCHSETVAR=PATCHSET_${PROJVAR}
 PATCHSET=${!PATCHSETVAR:-master}
-REPO=https://gerrit.named-data.net/$PROJ
+REPO=https://gerrit.named-data.net/${PROJ}
 
 if [[ ${PATCHSET} == master ]] || [[ ${PATCHSET} == skip ]]; then
   # use default branch
   GERRIT_BRANCH=
 else
   GERRIT_CHANGE=${PATCHSET%,*}
-  GERRIT_BRANCH=refs/changes/$(printf '%02d' $((GERRIT_CHANGE % 100)))/${GERRIT_CHANGE}/${PATCHSET#*,}
+  GERRIT_BRANCH=refs/changes/${GERRIT_CHANGE: -2}/${GERRIT_CHANGE}/${PATCHSET#*,}
 fi
 
 echo "Checking out $REPO ${GERRIT_BRANCH:-(default branch)}"
 git init --quiet
 git fetch --quiet --depth=1 "$REPO" "$GERRIT_BRANCH"
-git -c advice.detachedHead=no checkout FETCH_HEAD
-git submodule update --init
+git switch --detach FETCH_HEAD
+git submodule --quiet update --init

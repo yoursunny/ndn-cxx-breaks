cdfc3f223b3d523d22f9a63f460c17a836bd03c1
Davide Pesavento <davidepesa@gmail.com>
2024-02-19 02:01:51 -0500

    NFD: unit-tests-rib is gone

--- NFD.sh
+++ NFD.sh
@@ -16,8 +16,5 @@ build/unit-tests-core
 echo Running NFD tests: daemon
 build/unit-tests-daemon
 
-echo Running NFD tests: rib
-build/unit-tests-rib
-
 echo Running NFD tests: tools
 build/unit-tests-tools
